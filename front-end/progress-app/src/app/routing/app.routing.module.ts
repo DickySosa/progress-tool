e0f7da24d6f97e9e7fa601caede60c72eafcab72
DickySosa <dicky5217@gmail.com>
2023-10-09 09:42:09 -0600

    fixing bug

--- front-end/progress-app/src/app/routing/app.routing.module.ts
+++ front-end/progress-app/src/app/routing/app.routing.module.ts
@@ -5,6 +5,7 @@ import { BodyMeasurementsComponent } from "../pages/info/body-measurements/body-
 import { BasalMetabolicRateComponent } from "../pages/calculators/basal-metabolic-rate/basal-metabolic-rate.component";
 import { BodyFatPercentageComponent } from "../pages/calculators/body-fat-percentage/body-fat-percentage.component";
 import { MuscleMassIndexComponent } from "../pages/calculators/muscle-mass-index/muscle-mass-index.component";
+import { ButtonComponent } from "../components/button/button.component";
 
 
 const routes:Routes = [
@@ -13,12 +14,13 @@ const routes:Routes = [
     {path: 'body-measurements', component: BodyMeasurementsComponent},
     {path: 'basal-metabolic-rate', component: BasalMetabolicRateComponent},
     {path: 'body-fat-percentage', component: BodyFatPercentageComponent},
-    {path: 'muscle-mass-index', component: MuscleMassIndexComponent}
+    {path: 'muscle-mass-index', component: MuscleMassIndexComponent},
 ]
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    declarations: []
 })
 
 export class AppRoutingModule {}
