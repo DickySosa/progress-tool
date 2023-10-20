5a4533a251aa19f1da31efb259f2ae40dc7f5e55
DickySosa <dicky5217@gmail.com>
2023-10-20 11:51:28 -0600

    Adding CommonModule inside body fat perentage module.ts

--- front-end/progress-app/src/app/pages/calculators/body-fat-percentage/body-fat-percentage.module.ts
+++ front-end/progress-app/src/app/pages/calculators/body-fat-percentage/body-fat-percentage.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { BodyFatPercentageRoutingModule } from "./body-fat-percentage.routing";
 import { BodyFatPercentageComponent } from "./body-fat-percentage.component";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
-    imports:[BodyFatPercentageRoutingModule],
+    imports:[BodyFatPercentageRoutingModule, CommonModule],
     declarations:[BodyFatPercentageComponent]
 })
 
