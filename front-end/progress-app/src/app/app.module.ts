f9e560d31422805081a2ba0946a3c7e8bb9c7040
DickySosa <dicky5217@gmail.com>
2023-12-08 19:15:12 -0600

    adding common module

--- front-end/progress-app/src/app/app.module.ts
+++ front-end/progress-app/src/app/app.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './routing/app.routing.module';
-// import { CommonModule } from '@angular/common';
-// import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -12,8 +11,7 @@ import { AppRoutingModule } from './routing/app.routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // FormsModule,
-    // CommonModule
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
